b09e73582f534388762243881af7da888e90a9f3
makvan <makvan@seas.upenn.edu>
2018-12-07 09:56:36 -0500

    adds games logic
    adds game logic and  new arrow resources

--- app/src/main/java/leadgames/cis400/leadgames/FishActivity.java
+++ app/src/main/java/leadgames/cis400/leadgames/FishActivity.java
@@ -1,163 +1,172 @@
 package leadgames.cis400.leadgames;
 
 import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
+
+
 public class FishActivity extends AppCompatActivity {
-    /**
-     * Whether or not the system UI should be auto-hidden after
-     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
-    private static final boolean AUTO_HIDE = true;
+    final int rightImgId = R.mipmap.righty;
+    final int leftImgId = R.mipmap.lefty;
 
-    /**
-     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-     * user interaction before hiding the system UI.
-     */
-    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
+    private ArrayList<ImageView> allFish;
+    private ArrayList<ImageView> leftFish;
+    private ArrayList<ImageView> rightFish;
+    private ImageView fish1;
+    private ImageView fish2;
+    private ImageView fish3;
+    private ImageView fish4;
+    private ImageView fish5;
 
-    /**
-     * Some older devices needs a small delay between UI widget updates
-     * and a change of the status and navigation bar.
-     */
-    private static final int UI_ANIMATION_DELAY = 300;
-    private final Handler mHideHandler = new Handler();
-    private View mContentView;
-    private final Runnable mHidePart2Runnable = new Runnable() {
-        @SuppressLint("InlinedApi")
-        @Override
-        public void run() {
-            // Delayed removal of status and navigation bar
+    private ImageView leftButton;
+    private ImageView rightButton;
 
-            // Note that some of these constants are new as of API 16 (Jelly Bean)
-            // and API 19 (KitKat). It is safe to use them, as they are inlined
-            // at compile-time and do nothing on earlier devices.
-            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-        }
-    };
-    private View mControlsView;
-    private final Runnable mShowPart2Runnable = new Runnable() {
-        @Override
-        public void run() {
-            // Delayed display of UI elements
-            ActionBar actionBar = getSupportActionBar();
-            if (actionBar != null) {
-                actionBar.show();
-            }
-            mControlsView.setVisibility(View.VISIBLE);
-        }
-    };
-    private boolean mVisible;
-    private final Runnable mHideRunnable = new Runnable() {
-        @Override
-        public void run() {
-            hide();
-        }
-    };
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
-    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
-        @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (AUTO_HIDE) {
-                delayedHide(AUTO_HIDE_DELAY_MILLIS);
-            }
-            return false;
-        }
-    };
+    private int wins = 0;
+    private int plays=0;
+    //LEFT = 0, RIGHT = 1;
+    private int midFishDir = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_fish);
+        init();
 
-        mVisible = true;
-//        mControlsView = findViewById(R.id.fullscreen_content_controls);
-//        mContentView = findViewById(R.id.fullscreen_content);
-
-
-        // Set up the user interaction to manually show or hide the system UI.
-        mContentView.setOnClickListener(new View.OnClickListener() {
+        fish4.setClickable(true);
+        fish4.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                toggle();
+            public void onClick(View v) {
+                if (midFishDir == 1) {
+                    wins += 1;
+                    Log.d("Played",  "win");
+                }
+                plays += 1;
+                shuffle();
             }
         });
 
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-//        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+        fish2.setClickable(true);
+        fish2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (midFishDir == 0) {
+                    wins += 1;
+                    Log.d("Played",  "win");
+                }
+                plays += 1;
+                shuffle();
+            }
+        });
+        return;
+
     }
 
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
+    private void init() {
+        fish1 = (ImageView) findViewById(R.id.fish1);
+        fish2 = (ImageView) findViewById(R.id.fish2);
+        fish3 = (ImageView) findViewById(R.id.fish3);
+        fish4 = (ImageView) findViewById(R.id.fish4);
+        fish5 = (ImageView) findViewById(R.id.fish5);
 
-        // Trigger the initial hide() shortly after the activity has been
-        // created, to briefly hint to the user that UI controls
-        // are available.
-        delayedHide(100);
+        allFish = new ArrayList<ImageView>();
+        allFish.add(fish1);
+        allFish.add(fish2);
+        allFish.add(fish3);
+        allFish.add(fish4);
+        allFish.add(fish5);
+
+        leftFish = new ArrayList<ImageView>();
+        leftFish.add(fish1);
+        leftFish.add(fish2);
+
+        rightFish = new ArrayList<ImageView>();
+        rightFish.add(fish4);
+        rightFish.add(fish5);
     }
 
-    private void toggle() {
-        if (mVisible) {
-            hide();
-        } else {
-            show();
+    private void restartGame() {
+        wins = 0;
+    }
+
+    private int setMidDirection() {
+        if (Math.random() < 0.5) {
+            midFishDir = 0;
+            return leftImgId;
         }
+        midFishDir = 1;
+        return rightImgId;
     }
-
-    private void hide() {
-        // Hide UI first
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.hide();
+    private int getDirection() {
+        if (Math.random() < 0.5) {
+            return leftImgId;
         }
-        mControlsView.setVisibility(View.GONE);
-        mVisible = false;
-
-        // Schedule a runnable to remove the status and navigation bar after a delay
-        mHideHandler.removeCallbacks(mShowPart2Runnable);
-        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
+        return rightImgId;
     }
 
-    @SuppressLint("InlinedApi")
-    private void show() {
-        // Show the system bar
-        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
-        mVisible = true;
-
-        // Schedule a runnable to display UI elements after a delay
-        mHideHandler.removeCallbacks(mHidePart2Runnable);
-        mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
-    }
-
-    /**
-     * Schedules a call to hide() in delay milliseconds, canceling any
-     * previously scheduled calls.
-     */
-    private void delayedHide(int delayMillis) {
-        mHideHandler.removeCallbacks(mHideRunnable);
-        mHideHandler.postDelayed(mHideRunnable, delayMillis);
+    private void shuffle() {
+        int dir1;
+        int dir2;
+        //all in same direction
+        if (wins < 4) {
+            dir1 = setMidDirection();
+            for (ImageView fish : allFish) {
+                fish.setImageResource(dir1);
+            }
+            return;
+        }
+        //surrounding in opposite side
+        if (wins < 7 ) {
+            dir1 = setMidDirection();
+            if (dir1 == rightImgId) {
+                dir2 = leftImgId;
+            } else {
+                dir2 = rightImgId;
+            }
+            fish3.setImageResource(dir1);
+            for (ImageView fish : leftFish) {
+                fish.setImageResource(dir2);
+            }
+            for (ImageView fish : rightFish) {
+                fish.setImageResource(dir2);
+            }
+            return;
+        }
+        //left and right not necessarily
+       if (wins < 11) {
+            dir1 = setMidDirection();
+            dir2 = getDirection();
+            int dir3 = getDirection();
+            fish3.setImageResource(dir1);
+           fish2.setImageResource(dir2);
+           fish4.setImageResource(dir2);
+           fish1.setImageResource(dir3);
+           fish5.setImageResource(dir3);
+            return;
+        }
+        //fully randomized
+        fish3.setImageResource(setMidDirection());
+        for (ImageView fish : leftFish) {
+            fish.setImageResource(getDirection());
+        }
+        for (ImageView fish : leftFish) {
+            fish.setImageResource(getDirection());
+        }
+        return;
     }
 }
