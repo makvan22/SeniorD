f9d6e460d21a7301cdb7283769e94decad77838b
Vanessa K. Makuvaro <makvan@seas.upenn.edu>
2019-03-29 14:53:59 -0400

    Adds scene class for game 3 and  outlines work to be done
    -creates structural trial class pending on researcher inputs
    - adds 2 available scences to xml and grass background
    - roughly outlines game three work remaining

--- app/src/main/java/leadgames/cis400/leadgames/GameThreeActivity.java
+++ app/src/main/java/leadgames/cis400/leadgames/GameThreeActivity.java
@@ -1,4 +1,45 @@
 package leadgames.cis400.leadgames;
 
-public class GameThreeActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+
+
+public class GameThreeActivity extends AppCompatActivity {
+
+    private ArrayList<StructuralTrial> Trails;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_three);
+        //TODO: init views
+        //TODO: run trials
+        //TODO: record trials / update database
+    }
+    
+    private final class Scene {
+        private ImageView imageView;
+        private boolean selected;
+        private boolean ambiguous;
+
+        public Scene (ImageView imageView, boolean ambiguous) {
+            this.selected = false;
+            this.ambiguous = ambiguous;
+            this.imageView = imageView;
+            imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                selected = !selected;
+                }
+            });
+        }
+
+        public boolean isAmbiguous() {
+            return ambiguous;
+        }
+    };
 }
---
+++ app/src/main/java/leadgames/cis400/leadgames/StructuralTrial.java
@@ -0,0 +1,5 @@
+package leadgames.cis400.leadgames;
+
+public class StructuralTrial {
+    //TODO: fetch inputs from researchers in order to model this class
+}
